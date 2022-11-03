180a491a0b810a63c539d47e3179cf8bd23b0a72
Juan Larrambebere <juanlarrambebere@192.168.0.104>
2022-11-03 16:57:51 -0300

    Created a class to standarize errors and a errorHandler middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import errorHandler from "./middlewares/errorHandler";
 
 const APP_PORT = process.env.APP_PORT;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandler);
 
 app.listen(APP_PORT, async () => {
   console.log(`Server running in http://localhost:${APP_PORT}`);
