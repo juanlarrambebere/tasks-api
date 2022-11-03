660c553a59eea87c50034282b743a54da812ef34
Juan Larrambebere <juanlarrambebere@192.168.0.104>
2022-11-03 17:38:10 -0300

    Implemented the missing logic to create users in the database

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 export const prismaClient = new PrismaClient();
+
+export const isUniqueConstraintViolation = (e: unknown) =>
+  e instanceof PrismaClientKnownRequestError && e.code === "P2002";
--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,6 +1,29 @@
+import argon2 from "argon2";
+import { BadRequestError } from "../errors";
+import { isUniqueConstraintViolation, prismaClient } from "../prisma/client";
 import { CreateUserInput } from "../schemas/createUserSchema";
 
 export const createUser = async (userInput: CreateUserInput) => {
-  // TODO implement me
-  return { id: 1 };
+  const hashedPassword = await argon2.hash(userInput.password);
+
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        username: userInput.username,
+        password: hashedPassword,
+      },
+    });
+
+    const { password, ...passwordlessUser } = user;
+
+    return passwordlessUser;
+  } catch (e) {
+    if (isUniqueConstraintViolation(e)) {
+      throw new BadRequestError(
+        "The username is already in use by another user"
+      );
+    }
+
+    throw e;
+  }
 };
